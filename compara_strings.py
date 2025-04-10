e6d2f67b0cd4096c3a0fa36d5adc9106a5e49a38
Julia-c-h <julia.hartmann.campos@escola.pr.gov.br>
2025-04-10 09:18:38 -0300

    Create compara_strings.py

---
+++ compara_strings.py
@@ -0,0 +1,9 @@
+# Solicita ao usuário que insira dois números
+letra1 = (input("Digite a 1a. letra: "))
+letra2 = (input("Digite a 2a. letra: "))
+
+# Verifica comparando se números são iguais
+if letra1 == letra2:
+    print("As letras são iguais.")
+else:
+    print("As letras são diferentes.")
